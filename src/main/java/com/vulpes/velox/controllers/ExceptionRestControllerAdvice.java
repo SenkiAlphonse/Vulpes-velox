d98b67c3f995695973b5305cd3124baced8e3af5
nilcsi <nilcsi@gmail.com>
2019-03-06 13:27:12 +0100

    Added ValidationErrorDto

--- src/main/java/com/vulpes/velox/controllers/ExceptionRestControllerAdvice.java
+++ src/main/java/com/vulpes/velox/controllers/ExceptionRestControllerAdvice.java
@@ -1,9 +1,13 @@
 package com.vulpes.velox.controllers;
 
 import com.vulpes.velox.dtos.ErrorResponseDto;
-import com.vulpes.velox.exceptions.BadEmailException;
+import com.vulpes.velox.exceptions.*;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
