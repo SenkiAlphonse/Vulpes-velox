e8e1788eaf50a0ef153cc7e52db44dd58078cb20
nilcsi <nilcsi@gmail.com>
2019-03-06 13:17:52 +0100

    BadRequestException added

--- src/main/java/com/vulpes/velox/controllers/ExceptionRestControllerAdvice.java
+++ src/main/java/com/vulpes/velox/controllers/ExceptionRestControllerAdvice.java
@@ -3,23 +3,53 @@ package com.vulpes.velox.controllers;
 import com.vulpes.velox.dtos.ErrorResponseDto;
 import com.vulpes.velox.exceptions.BadEmailException;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ExceptionRestControllerAdvice extends ResponseEntityExceptionHandler {
 
-  @ExceptionHandler(BadEmailException.class)
-  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @ExceptionHandler(NotFoundException.class)
+  @ResponseStatus(HttpStatus.NOT_FOUND)
   ErrorResponseDto notFoundHandler(
-      HttpServletRequest page, BadEmailException exception) {
+      HttpServletRequest page, NotFoundException exception) {
     return getErrorResponseDto(
         exception.getExceptionMessage(), page, HttpStatus.NOT_FOUND);
+  }
+
+  @ExceptionHandler(InternalServerErrorException.class)
+  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+  ErrorResponseDto internalServerErrorHandler(
+      HttpServletRequest page, InternalServerErrorException exception) {
+    return getErrorResponseDto(
+        exception.getExceptionMessage(), page, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  @ExceptionHandler(UnauthorizedException.class)
+  @ResponseStatus(HttpStatus.UNAUTHORIZED)
+  ErrorResponseDto unauthorizedHandler(
+      HttpServletRequest page, UnauthorizedException exception) {
+    return getErrorResponseDto(
+        exception.getExceptionMessage(), page, HttpStatus.UNAUTHORIZED);
+  }
+
+  @ExceptionHandler(ForbiddenException.class)
+  @ResponseStatus(HttpStatus.FORBIDDEN)
+  ErrorResponseDto forbiddenHandler(
+      HttpServletRequest page, ForbiddenException exception) {
+    return getErrorResponseDto(
+        exception.getExceptionMessage(), page, HttpStatus.FORBIDDEN);
+  }
+
+  @ExceptionHandler(BadRequestException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  ErrorResponseDto badRequestHandler(
+      HttpServletRequest page, BadRequestException exception) {
+    return getErrorResponseDto(
+        exception.getExceptionMessage(), page, HttpStatus.BAD_REQUEST);
   }
 
   private ErrorResponseDto getErrorResponseDto(
@@ -30,6 +60,30 @@ public class ExceptionRestControllerAdvice extends ResponseEntityExceptionHandle
     errorResponseDto.status = httpStatus;
     errorResponseDto.timeStamp = new Date();
     return errorResponseDto;
+  }
+
+  @Override
+  protected ResponseEntity<Object> handleMethodArgumentNotValid(
+      MethodArgumentNotValidException exception,
+      HttpHeaders headers,
+      HttpStatus status,
+      WebRequest webRequest) {
+    ValidationErrorDto validationErrorDto = getDtoFromExceptionErrors(exception.getBindingResult());
+
+    return super.handleExceptionInternal(
+        exception, validationErrorDto, headers, status, webRequest);
+  }
+
+  private ValidationErrorDto getDtoFromExceptionErrors(Errors errors) {
+    ValidationErrorDto validationErrorDto = new ValidationErrorDto();
+    List<String> errorsFound = new ArrayList<>();
+
+    for (ObjectError objectError : errors.getAllErrors()) {
+      errorsFound.add(objectError.getDefaultMessage());
+    }
+    validationErrorDto.status = "error";
+    validationErrorDto.message = errorsFound;
+    return validationErrorDto;
   }
 
 }
