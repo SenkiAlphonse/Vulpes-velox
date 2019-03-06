da03dc67f22724eb32c109cb771066ef52e49b40
nilcsi <nilcsi@gmail.com>
2019-03-06 12:38:13 +0100

    Added controller advice

---
+++ src/main/java/com/vulpes/velox/controllers/ExceptionRestControllerAdvice.java
@@ -0,0 +1,8 @@
+package com.vulpes.velox.controllers;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class ExceptionRestControllerAdvice extends ResponseEntityExceptionHandler {
+  }
