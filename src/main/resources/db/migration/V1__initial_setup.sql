338438a409247de97cb7de37d0477283d9ed0689
nilcsi <nilcsi@gmail.com>
2019-03-06 12:16:01 +0100

    Working migration file

--- src/main/resources/db/migration/V1__initial_setup.sql
+++ src/main/resources/db/migration/V1__initial_setup.sql
@@ -1,32 +1,33 @@
-CREATE TABLE bulk_products (
-  shipments VARCHAR(255)
-)
-
-CREATE TABLE shipments (
-  id BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-  quantity BIGINT,
-  arrival DATE,
-  best_before DATE,
-  bulk_product_id BIGINT,
-  FOREIGN KEY (bulk_product_id) REFERENCES bulk_products (id)
-)
-
-CREATE TABLE items (
-  id BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-  product_number BIGINT
-  identified_product_id BIGINT,
-  FOREIGN KEY (identified_product_id) REFERENCES identified_products (id)
-)
+CREATE TABLE product_ids (
+  product_id          BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL
+);
 
 CREATE TABLE identified_products (
-  items VARCHAR(255)
-)
+  id                  BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  name                VARCHAR(255)
+);
 
-CREATE TABLE orders (
-
-)
+CREATE TABLE bulk_products (
+  id                  BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  name                VARCHAR(255)
+);
 
 CREATE TABLE products (
-  id BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-  name VARCHAR(255)
-)
+  id                  BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  name                VARCHAR(255),
+  dtype               VARCHAR(255)
+);
+
+CREATE TABLE items (
+  id                  BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  product_number      VARCHAR(255),
+  identified_product_id  BIGINT
+);
+
+CREATE TABLE product_without_id_items (
+  id                  BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  quantity            BIGINT,
+  arrival             VARCHAR(255),
+  best_before         VARCHAR(255),
+  bulk_product_id VARCHAR(255)
+);
