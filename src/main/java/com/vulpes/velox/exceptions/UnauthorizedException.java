8c1ad64a01c876c4ce13cee5d86f4dbe594c7cec
nilcsi <nilcsi@gmail.com>
2019-03-06 13:25:36 +0100

    Added UnauthorizedException

--- src/main/java/com/vulpes/velox/exceptions/UnauthorizedException.java
+++ src/main/java/com/vulpes/velox/exceptions/UnauthorizedException.java
@@ -1,4 +1,18 @@
 package com.vulpes.velox.exceptions;
 
-public class UnauthorizedException {
+public class UnauthorizedException extends RuntimeException {
+
+  private String exceptionMessage;
+
+  public UnauthorizedException(String exceptionMessage) {
+    this.exceptionMessage = exceptionMessage;
+  }
+
+  public String getExceptionMessage() {
+    return exceptionMessage;
+  }
+
+  public void setExceptionMessage(String exceptionMessage) {
+    this.exceptionMessage = exceptionMessage;
+  }
 }
