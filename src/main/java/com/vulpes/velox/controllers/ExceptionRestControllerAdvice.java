8c48c700ed57faef8f7a9a5670274f2779480891
nilcsi <nilcsi@gmail.com>
2019-03-06 12:49:03 +0100

    Changed RestControllerAdvice to ControllerAdvice

--- src/main/java/com/vulpes/velox/controllers/ExceptionRestControllerAdvice.java
+++ src/main/java/com/vulpes/velox/controllers/ExceptionRestControllerAdvice.java
@@ -1,23 +1,35 @@
 package com.vulpes.velox.controllers;
 
+import com.vulpes.velox.dtos.ErrorResponseDto;
 import com.vulpes.velox.exceptions.BadEmailException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
 
-@RestControllerAdvice
+@ControllerAdvice
 public class ExceptionRestControllerAdvice extends ResponseEntityExceptionHandler {
 
   @ExceptionHandler(BadEmailException.class)
-  @ResponseStatus(HttpStatus.NOT_FOUND)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
   ErrorResponseDto notFoundHandler(
       HttpServletRequest page, BadEmailException exception) {
     return getErrorResponseDto(
         exception.getExceptionMessage(), page, HttpStatus.NOT_FOUND);
+  }
+
+  private ErrorResponseDto getErrorResponseDto(
+      String exceptionMessage, HttpServletRequest page, HttpStatus httpStatus) {
+    ErrorResponseDto errorResponseDto = new ErrorResponseDto();
+    errorResponseDto.path = page.getRequestURI();
+    errorResponseDto.message = exceptionMessage;
+    errorResponseDto.status = httpStatus;
+    errorResponseDto.timeStamp = new Date();
+    return errorResponseDto;
   }
 
 }
