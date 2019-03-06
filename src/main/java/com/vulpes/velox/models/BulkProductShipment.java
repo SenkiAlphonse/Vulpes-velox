50da13a57ec2001e6e699aa1c38b9112b9e209fd
Bali <bali332211@freemail.com>
2019-03-06 11:36:32 +0100

    added fields

--- src/main/java/com/vulpes/velox/models/BulkProductShipment.java
+++ src/main/java/com/vulpes/velox/models/BulkProductShipment.java
@@ -1,4 +1,22 @@
 package com.vulpes.velox.models;
 
+import javax.persistence.*;
+import java.time.Instant;
+
+@Entity
+@Table(name = "bulk_product_shipments")
 public class BulkProductShipment {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private Long quantity;
+  private Instant arrival;
+  @Column(name = "best_before")
+  private Instant bestBefore;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "product_without_id_id")
+  private BulkProduct bulkProduct;
 }
