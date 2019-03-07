a2c821e4b8de53984c1e5611d730d7e189dd7b76
SenkiAlphonse <the.nagy.kriszta@gmail.com>
2019-03-07 11:38:08 +0100

    set up vars and postgresql stuff

--- src/main/resources/db/migration/V1__initial_setup.sql
+++ src/main/resources/db/migration/V1__initial_setup.sql
@@ -1,29 +1,29 @@
 CREATE TABLE identified_products (
-  id                  BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  id                  BIGSERIAL PRIMARY KEY NOT NULL,
   name                VARCHAR(255)
 );
 
 CREATE TABLE bulk_products (
-  id                  BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  id                  BIGSERIAL PRIMARY KEY NOT NULL,
   name                VARCHAR(255)
 );
 
 CREATE TABLE products (
-  id                  BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  id                  BIGSERIAL PRIMARY KEY NOT NULL,
   name                VARCHAR(255),
   quantity            BIGINT,
   dtype               VARCHAR(255)
 );
 
 CREATE TABLE items (
-  id                  BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  id                  BIGSERIAL PRIMARY KEY NOT NULL,
   product_number      VARCHAR(255),
   identified_product_id     BIGINT,
   FOREIGN KEY (identified_product_id) REFERENCES identified_products(id)
 );
 
 CREATE TABLE shipments (
-  id                  BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  id                  BIGSERIAL PRIMARY KEY NOT NULL,
   quantity            BIGINT,
   arrival             VARCHAR(255),
   best_before         VARCHAR(255),
@@ -32,13 +32,13 @@ CREATE TABLE shipments (
 );
 
 CREATE TABLE orders (
-  id                  BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  id                  BIGSERIAL PRIMARY KEY NOT NULL,
   name                VARCHAR(255),
   date                VARCHAR(255)
 );
 
 CREATE TABLE ordered_products (
-  id                  BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  id                  BIGSERIAL PRIMARY KEY NOT NULL,
   product_name            VARCHAR(255),
   quantity                BIGINT,
   order_id            BIGINT,
@@ -46,7 +46,7 @@ CREATE TABLE ordered_products (
 );
 
 CREATE TABLE users (
-                         id                  BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-                         email               VARCHAR(255),
-                         is_god              BOOL
+  id                  BIGSERIAL PRIMARY KEY NOT NULL,
+  email               VARCHAR(255),
+  is_god              BOOL
 );
