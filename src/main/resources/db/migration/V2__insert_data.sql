e213b51356c3a04823c031c19af8a352e940c2eb
SenkiAlphonse <the.nagy.kriszta@gmail.com>
2019-03-07 12:58:22 +0100

    added god users for all contributors in flyway

--- src/main/resources/db/migration/V2__insert_data.sql
+++ src/main/resources/db/migration/V2__insert_data.sql
@@ -1,3 +1,5 @@
 INSERT INTO users(email, is_god)
 VALUES ('kalendaryoii@gmail.com', false),
-       ('the.nagy.kriszta@gmail.com', true);
+       ('the.nagy.kriszta@gmail.com', true),
+       ('bali332211bali@gmail.com', true),
+       ('nilcsi@gmail.com', true);
