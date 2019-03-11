1f13fae41e78125c60f6b1f5893ce8aaa4b96565
SenkiAlphonse <the.nagy.kriszta@gmail.com>
2019-03-11 11:24:19 +0100

    flyway v2 adjust tablename

--- src/main/resources/db/migration/V2__insert_data.sql
+++ src/main/resources/db/migration/V2__insert_data.sql
@@ -1,4 +1,4 @@
-INSERT INTO d6a9rjo53hbtv3.users(email, is_god)
+INSERT INTO users(email, is_god)
 VALUES ('kalendaryoii@gmail.com', false),
        ('the.nagy.kriszta@gmail.com', true),
        ('bali332211bali@gmail.com', true),
