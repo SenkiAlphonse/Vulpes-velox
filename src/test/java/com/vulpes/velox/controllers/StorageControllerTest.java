03a868327c76af264a38605f8beee6a1ade7029b
Bali <bali332211@freemail.com>
2019-03-20 13:59:54 +0100

    added test for saved new item field

--- src/test/java/com/vulpes/velox/controllers/StorageControllerTest.java
+++ src/test/java/com/vulpes/velox/controllers/StorageControllerTest.java
@@ -11,16 +11,21 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.mockito.ArgumentCaptor;
 
 import static org.apache.commons.lang3.StringUtils.contains;
-import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.hamcrest.Matchers.hasProperty;
 import static org.hamcrest.Matchers.nullValue;
+
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebMvcTest(controllers = StorageController.class, secure = false)
@@ -71,6 +76,14 @@ public class StorageControllerTest {
 //        .andExpect(model().attribute("itemNew", hasProperty("id", nullValue())));
 //        .andExpect(model().attributeExists("itemNew"));
 //        .andExpect(model().attribute("itemNew", is(itemNew)));
+
+    ArgumentCaptor<Item> itemArgument = ArgumentCaptor.forClass(Item.class);
+    verify(itemService, times(1)).save(itemArgument.capture());
+    verify(itemService, times(1)).getAllByIdentifiedProduct(identifiedProduct);
+    verifyNoMoreInteractions(itemService);
+
+    Item itemArgumentValue = itemArgument.getValue();
+    assertThat(itemArgumentValue.getIdentifiedProduct(), is(identifiedProduct));
   }
 
 
