2f7e871969a4d9adb25e3c4030233972f176de82
SenkiAlphonse <the.nagy.kriszta@gmail.com>
2019-03-27 14:24:32 +0100

    added postgres properties and sql syntax

--- src/main/resources/db/migration/V1__initial_setup.sql
+++ src/main/resources/db/migration/V1__initial_setup.sql
@@ -1,42 +1,52 @@
+CREATE TABLE identified_products (
+                                   id                  BIGSERIAL PRIMARY KEY NOT NULL,
+                                   name                VARCHAR(255)
+);
+
+CREATE TABLE bulk_products (
+                             id                  BIGSERIAL PRIMARY KEY NOT NULL,
+                             name                VARCHAR(255)
+);
+
 CREATE TABLE products (
-  id                  BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-  name                VARCHAR(255),
-  quantity            BIGINT,
-  dtype               VARCHAR(255)
+                        id                  BIGSERIAL PRIMARY KEY NOT NULL,
+                        name                VARCHAR(255),
+                        quantity            BIGINT,
+                        dtype               VARCHAR(255)
 );
 
 CREATE TABLE items (
-  id                  BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-  product_number      VARCHAR(255),
-  identified_product_id     BIGINT,
-  FOREIGN KEY (identified_product_id) REFERENCES products(id)
+                     id                  BIGSERIAL PRIMARY KEY NOT NULL,
+                     product_number      VARCHAR(255),
+                     identified_product_id     BIGINT,
+                     FOREIGN KEY (identified_product_id) REFERENCES products(id)
 );
 
 CREATE TABLE shipments (
-  id                  BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-  quantity            BIGINT,
-  arrival             VARCHAR(255),
-  best_before         VARCHAR(255),
-  bulk_product_id     BIGINT,
-  FOREIGN KEY (bulk_product_id) REFERENCES products(id)
+                         id                  BIGSERIAL PRIMARY KEY NOT NULL,
+                         quantity            BIGINT,
+                         arrival             VARCHAR(255),
+                         best_before         VARCHAR(255),
+                         bulk_product_id     BIGINT,
+                         FOREIGN KEY (bulk_product_id) REFERENCES products(id)
 );
 
 CREATE TABLE orders (
-  id                  BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-  name                VARCHAR(255),
-  date                VARCHAR(255)
+                      id                  BIGSERIAL PRIMARY KEY NOT NULL,
+                      name                VARCHAR(255),
+                      date                VARCHAR(255)
 );
 
 CREATE TABLE ordered_products (
-  id                  BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-  product_name            VARCHAR(255),
-  quantity                BIGINT,
-  order_id            BIGINT,
-  FOREIGN KEY (order_id) REFERENCES orders(id)
+                                id                  BIGSERIAL PRIMARY KEY NOT NULL,
+                                product_name            VARCHAR(255),
+                                quantity                BIGINT,
+                                order_id            BIGINT,
+                                FOREIGN KEY (order_id) REFERENCES orders(id)
 );
 
 CREATE TABLE users (
-                         id                  BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-                         email               VARCHAR(255),
-                         is_god              BOOL
+                     id                  BIGSERIAL PRIMARY KEY NOT NULL,
+                     email               VARCHAR(255),
+                     is_god              BOOL
 );
