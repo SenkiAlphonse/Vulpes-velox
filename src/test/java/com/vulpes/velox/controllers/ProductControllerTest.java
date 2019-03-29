78a7265581aea5eb24d3881c94a0a141a54a90fa
giba86 <giba86@gmail.com>
2019-03-29 12:04:57 +0100

    test started but not finished yet

--- src/test/java/com/vulpes/velox/controllers/ProductControllerTest.java
+++ src/test/java/com/vulpes/velox/controllers/ProductControllerTest.java
@@ -1,6 +1,6 @@
 package com.vulpes.velox.controllers;
 
-import com.vulpes.velox.models.Item;
+import com.vulpes.velox.config.WebSecurityConfig;
 import com.vulpes.velox.models.products.IdentifiedProduct;
 import com.vulpes.velox.services.itemservice.ItemService;
 import com.vulpes.velox.services.productservice.ProductService;
@@ -8,28 +8,37 @@ import com.vulpes.velox.services.userservice.UserService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
 
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.*;
+import java.nio.charset.Charset;
+
+import static io.restassured.authentication.FormAuthConfig.springSecurity;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
-import static org.mockito.internal.verification.VerificationModeFactory.times;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebMvcTest(controllers = ProductController.class, secure = false)
 public class ProductControllerTest {
+
+  private MediaType contentType = new MediaType(MediaType.APPLICATION_FORM_URLENCODED.getType(),
+          MediaType.APPLICATION_FORM_URLENCODED.getSubtype(),
+          Charset.forName("utf8"));
+
+  private MockMvc mockMvc;
+
   @Autowired
-  MockMvc mockMvc;
+  private WebSecurityConfig webSecurityConfig;
+
+  @Autowired
+  private WebApplicationContext webApplicationContext;
 
   @MockBean
   private ProductService productService;
@@ -42,7 +51,9 @@ public class ProductControllerTest {
 
   @Before
   public void setup() {
-    identifiedProduct = new IdentifiedProduct();
+//    mockMvc = webAppContextSetup(webApplicationContext)
+//            .apply(springSecurity())
+//            .build();
   }
 
   @Test
