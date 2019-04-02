fd38f42997f5be480abd8225ec0e90f32fe967fb
Bali <bali332211@freemail.com>
2019-04-02 14:35:36 +0200

    changed ItemServiceTest

--- src/test/java/com/vulpes/velox/services/ItemServiceTest.java
+++ src/test/java/com/vulpes/velox/services/ItemServiceTest.java
@@ -1,23 +1,35 @@
 package com.vulpes.velox.services;
 
 import com.vulpes.velox.VeloxApplication;
+import com.vulpes.velox.models.Item;
 import com.vulpes.velox.models.products.IdentifiedProduct;
+import com.vulpes.velox.repositories.ItemRepository;
 import com.vulpes.velox.services.identifiedproductservice.IdentifiedProductService;
 import com.vulpes.velox.services.itemservice.ItemService;
+import com.vulpes.velox.services.itemservice.ItemServiceImpl;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.powermock.reflect.Whitebox;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.spy;
+import static org.powermock.api.mockito.PowerMockito.doReturn;
+import static org.powermock.api.mockito.PowerMockito.when;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @RunWith(SpringRunner.class)
@@ -33,8 +45,13 @@ public class ItemServiceTest {
   @Autowired
   private IdentifiedProductService identifiedProductService;
 
+  @MockBean
+  private RedirectAttributes redirectAttributes;
+
   private List<IdentifiedProduct> identifiedProducts;
   private IdentifiedProduct identifiedProduct;
+  private Map<String, Boolean> map;
+
 
   @Before
   public void setup() {
@@ -50,10 +67,10 @@ public class ItemServiceTest {
   }
 
 //  @Test
-//  void save() {
+//  public void save() {
 //
 //  }
-//
+
 //  @Test
 //  void getAll() {
 //
@@ -63,12 +80,25 @@ public class ItemServiceTest {
 //  void existsByProductNumber() {
 //
 //  }
-//
+
 //  @Test
-//  void getErrorFlashAttributes() {
+//  public void getErrorFlashAttributes() throws Exception {
+//    ItemServiceImpl itemServiceMock = spy(new ItemServiceImpl());
+//    doReturn(Collections.emptyMap()).when(itemServiceMock,
+//        "getErrorMessageFlashAttributes",
+//        any(), any());
+//    when(itemServiceMock, "getErrorFlashAttributes",
+//        any(), any(), any())
+//        .thenReturn(Collections.emptyMap());
 //
+////    assertThat(Whitebox.invokeMethod(new ItemServiceImpl(),
+////        "getErrorMessageFlashAttributes", "message", any(RedirectAttributes.class)), instanceOf(Map.class));
+//    assertThat(
+//        itemService.getErrorFlashAttributes(
+//            "name", new Item(), redirectAttributes),
+//        is(Collections.emptyMap()));
 //  }
-//
+
 //  @Test
 //  void getNewItemFlashAttributes() {
 //
