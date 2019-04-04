da89f9f5aaefdfdd2d4489cdfa0030da4643d814
Bali <bali332211@freemail.com>
2019-04-04 13:59:06 +0200

    added bulkProductNewWithoutName test

--- src/test/java/com/vulpes/velox/controllers/BulkProductControllerTest.java
+++ src/test/java/com/vulpes/velox/controllers/BulkProductControllerTest.java
@@ -9,6 +9,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
+import org.springframework.beans.NullValueInNestedPathException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -18,8 +19,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
 import static org.mockito.ArgumentMatchers.isNull;
 import static org.mockito.ArgumentMatchers.notNull;
@@ -37,15 +41,16 @@ public class BulkProductControllerTest {
   MockMvc mockMvc;
 
   @MockBean
-  private ProductService productService;
-  @MockBean
   private BulkProductService bulkProductService;
   @MockBean
   private UserService userService;
 
+  private Map<String, Boolean> errorFlashAttributes;
+
   @Before
   public void setup() {
-
+    errorFlashAttributes = new HashMap<>();
+    errorFlashAttributes.put("itemError", true);
   }
 
   @Test
@@ -83,7 +88,33 @@ public class BulkProductControllerTest {
     assertThat(bulkProductArgumentValue.getId(), is((long) 1));
     assertThat(bulkProductArgumentValue.getName(), is("Name"));
     assertThat(stringArgumentValue, is("Piece"));
+  }
 
+  @Test
+  public void bulkProductNewWithoutName() throws Exception {
+    when(userService.isUser(isNull())).thenReturn(true);
+    when((Object) bulkProductService.getErrorFlashAttributes(
+        notNull(), notNull())).thenReturn(errorFlashAttributes);
+
+    mockMvc.perform(post("/bulkProduct/new")
+        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+        .param("id", "1")
+        .param("unitToSet", "Piece")
+    )
+        .andDo(print())
+        .andExpect(status().isFound())
+        .andExpect(redirectedUrl("/storage/add"))
+        .andExpect(view().name("redirect:/storage/add"));
+
+    ArgumentCaptor<BulkProduct> bulkProductArgument = ArgumentCaptor.forClass(BulkProduct.class);
+
+    verify(bulkProductService, times(1)).getErrorFlashAttributes(
+        bulkProductArgument.capture(), any(RedirectAttributes.class));
+    verifyNoMoreInteractions(bulkProductService);
+
+    BulkProduct bulkProductArgumentValue = bulkProductArgument.getValue();
+    assertThat(bulkProductArgumentValue.getId(), is((long) 1));
+    assertThat(bulkProductArgumentValue.getName(), is(nullValue()));
   }
 
 }
