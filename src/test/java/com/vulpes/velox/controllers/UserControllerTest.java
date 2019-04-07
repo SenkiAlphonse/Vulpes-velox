3c4bff4a3546bb882db63243e36b3e7559c69fc5
Bali <bali332211@freemail.com>
2019-04-07 12:49:17 +0200

    added tests for userService

--- src/test/java/com/vulpes/velox/controllers/UserControllerTest.java
+++ src/test/java/com/vulpes/velox/controllers/UserControllerTest.java
@@ -1,6 +1,7 @@
 package com.vulpes.velox.controllers;
 
 import com.vulpes.velox.models.Shipment;
+import com.vulpes.velox.models.User;
 import com.vulpes.velox.services.bulkproductservice.BulkProductService;
 import com.vulpes.velox.services.productservice.ProductService;
 import com.vulpes.velox.services.shipmentservice.ShipmentService;
@@ -133,7 +134,85 @@ public class UserControllerTest {
         .andExpect(model().attributeDoesNotExist("islastpage"))
         .andExpect(model().attribute("unauthorizedEmail", "email"))
         .andExpect(view().name("unauthorized"));
+
+    verify(userService, times(1)).isUser(isNull());
+    verify(userService, times(1)).getUserEmail(isNull());
+    verifyNoMoreInteractions(userService);
   }
+
+  @Test
+  public void usersNew() throws Exception {
+    when(userService.isAdmin(isNull())).thenReturn(true);
+    when((Object) userService.getErrorFlashAttributes(
+        notNull(), notNull())).thenReturn(Collections.emptyMap());
+
+    mockMvc.perform(post("/users")
+        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+        .param("id", "1")
+        .param("email", "Email")
+        .param("name", "Name")
+    )
+        .andDo(print())
+        .andExpect(status().isFound())
+        .andExpect(redirectedUrl("/users"))
+        .andExpect(view().name("redirect:/users"));
+
+    ArgumentCaptor<User> userArgument = ArgumentCaptor.forClass(User.class);
+    verify(userService, times(1)).isAdmin(isNull());
+    verify(userService, times(1)).addUser(userArgument.capture());
+    verify(userService, times(1))
+        .getErrorFlashAttributes(any(RedirectAttributes.class), any(User.class));
+    verifyNoMoreInteractions(userService);
+
+    User userArgumentValue = userArgument.getValue();
+    assertThat(userArgumentValue.getId(), is((long) 1));
+    assertThat(userArgumentValue.getEmail(), is("Email"));
+    assertThat(userArgumentValue.getName(), is("Name"));
+  }
+
+  @Test
+  public void usersNewEmailTaken() throws Exception {
+    when(userService.isAdmin(isNull())).thenReturn(true);
+    when((Object) userService.getErrorFlashAttributes(
+        notNull(), notNull())).thenReturn(errorFlashAttributes);
+
+    mockMvc.perform(post("/users")
+        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+        .param("id", "1")
+        .param("email", "Taken")
+        .param("name", "Name")
+    )
+        .andDo(print())
+        .andExpect(status().isFound())
+        .andExpect(redirectedUrl("/users#adduser"))
+        .andExpect(view().name("redirect:/users#adduser"));
+
+    verify(userService, times(1)).isAdmin(isNull());
+    verify(userService, times(1))
+        .getErrorFlashAttributes(any(RedirectAttributes.class), any(User.class));
+    verifyNoMoreInteractions(userService);
+  }
+
+  @Test
+  public void usersNewUnauthorized() throws Exception {
+    when(userService.isAdmin(isNull())).thenReturn(false);
+
+    mockMvc.perform(post("/users")
+    )
+        .andDo(print())
+        .andExpect(status().isUnauthorized())
+        .andExpect(jsonPath("$.status", is("UNAUTHORIZED")))
+        .andExpect(jsonPath("$.message", is(
+            "Request denied, admin role is required to add users.")))
+        .andExpect(jsonPath("$.path", is("/users")))
+        .andExpect(jsonPath("$.timeStamp", containsString("")));
+
+    verify(userService, times(1)).isAdmin(isNull());
+    verifyNoMoreInteractions(userService);
+  }
+
+
+
 
 
 
